7d3049e0d1751e33231028dc9273f90ebab73e1e
wonhyejong <whj0524@naver.com>
2022-07-11 14:15:27 +0900

    프라이빗페이지 리다이렉트

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,16 +2,21 @@ import React from 'react';
 import "./ProductItem.css";
 import { useDispatch } from "react-redux";
 import { addCart } from '../store/cart/cartSlice';
+import { useNavigate } from 'react-router-dom';
 
 const ProductItem = ({item}) => {
+    const navigate = useNavigate();
     const dispatch=useDispatch();
+    const goToDetail=()=>{
+        navigate(`/product/${item.id}`)
+    }
     return (
         <div className="product-item">
             <img
             className="item-image" 
             alt="product"
             src={item.product_img} />
-            <div className="item-name">{item.product_name}</div>
+            <div className="item-name" onClick={goToDetail}>{item.product_name}</div>
             <div className="item-price">{item.price.toLocaleString()}원</div>
             <button className="add-cart-btn" onClick={()=>dispatch(addCart(item))}>
                 <i className="fas fa-plus"></i>
