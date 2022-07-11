746ce68d893eade53adc3a699fcbb34ae00e037e
wonhyejong <whj0524@naver.com>
2022-07-11 11:46:17 +0900

    redux-persist 세팅

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import store from './store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from "redux-persist";
 
+let persistor = persistStore(store) ;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-     <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
+   
   </React.StrictMode>
 );
 
